2db2677e9cc51e660ed22d408c7ec90d88c649b3
primaryuser <13755487+omar-nav@users.noreply.github.com>
2018-02-13 14:45:31 -0600

    eliminate un-used styling

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var data = [455, 377, 40, 77, 281, 86, 303, 337, 365, 110];
+var numbers = [177, 1177, 377, 77, 281, 86, 303, 337, 365, 110];
 
-data.sort(function(a, b){return a-b});
+numbers.sort(function(a, b){return a-b});
+
 d3.select(".chart")
   .selectAll("div")
-  .data(data)
+  .data(numbers)
     .enter()
     .append("div")
     .style("width", function(d) { return d + "px"; })
-    .text(function(d) { return '\u00A3 ' + d; });
+    .text(function(d) { return "\u00A3 " + d.toLocaleString(); });
---
+++ table.js
@@ -0,0 +1,73 @@
+// a script for descriptive stats
+// and showing them in a table
+
+var numbers = [177, 1177, 377, 77, 281, 86, 303, 337, 365, 110];
+
+function mean(numbers) {
+    var total = 0, i;
+    for (i = 0; i < numbers.length; i += 1) {
+        total += numbers[i];
+    }
+    return total / numbers.length;
+}
+function sum(numbers) {
+    var total = 0, i;
+    for (i = 0; i < numbers.length; i += 1) {
+        total += numbers[i];
+    }
+    return total;
+}
+
+function median(numbers) {
+    var median = 0, numsLen = numbers.length;
+    numbers.sort();
+    if (
+        numsLen % 2 === 0 // is even
+    ) {
+        // average of two middle numbers
+        median = (numbers[numsLen / 2 - 1] + numbers[numsLen / 2]) / 2;
+    } else { // is odd
+        // middle number only
+        median = numbers[(numsLen - 1) / 2];
+    }
+    return median;
+}
+
+function generate_table() {
+    // get the reference for the body
+    var body = document.getElementsByTagName("body")[0];
+
+    // creates a <table> element and a <tbody> element
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+
+    // creating all cells
+    for (var i = 0; i < 2; i++) {
+        // creates a table row
+        var row = document.createElement("tr");
+
+        for (var j = 0; j < 2; j++) {
+            // Create a <td> element and a text node, make the text
+            // node the contents of the <td>, and put the <td> at
+            // the end of the table row
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode("cell in row " + i + ", column " + j);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+
+        // add the row to the end of the table body
+        tblBody.appendChild(row);
+    }
+
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    body.appendChild(tbl);
+    // sets the border attribute of tbl to 2;
+    tbl.setAttribute("border", "2");
+}
+
+var avg = mean(numbers);
+var sum = sum(numbers);
+var median = median(numbers);
